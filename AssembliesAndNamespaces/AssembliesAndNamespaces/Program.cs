05949e79113649ebcbb04636491896a23e47abbe
Carlos-Frontend-nts <crodriguez@neural.com.do>
2022-08-22 16:41:34 -0400

    Tutorial completed

---
+++ AssembliesAndNamespaces/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
+
+            Console.WriteLine(reply);
+            File.WriteAllText(@"C:\Lesson17\WriteText", reply);
+            Console.ReadLine();
+        }
+    }
+}
